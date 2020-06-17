8477566aa4a9bf43ad7389b3e082315a3ff3d248
Sujith-shastry <sujithgopinatha08@gmail.com>
2020-06-17 16:34:12 +0530

    Jun17

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import os
+import smtplib
+from flask_mail import Mail
+
 app = Flask(__name__)
 app = Flask(__name__, template_folder='template')
 SECRET_KEY = os.urandom(32)
@@ -11,7 +14,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view='login'
-login_manager.login_message_category='info'
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,10 +2,11 @@ import os
 import secrets
 from PIL import Image
 from flask import Flask, render_template ,url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
-from flaskblog.forms import Registrationform, Loginform, UpdateAccountform, Postform
+from flaskblog import app, db, bcrypt, mail
+from flaskblog.forms import Registrationform, Loginform, UpdateAccountform, Postform,ResetPasswordForm,RequestResetForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 @app.route("/")
 
@@ -15,14 +16,16 @@ def home():
    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
    return render_template('home.html',posts=posts)
 
+
 @app.route("/about" ,methods=['GET', 'POST'])
 def about():
    return render_template('about.html',title='About Page')
 
+
 @app.route("/register" , methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+      return redirect(url_for('home'))
     form = Registrationform()
     if form.validate_on_submit():
       hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -33,10 +36,11 @@ def register():
       return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route("/login",methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+      return redirect(url_for('home'))
     form = Loginform()
     if form.validate_on_submit():
       user = User.query.filter_by(email=form.email.data).first()
@@ -48,22 +52,24 @@ def login():
         flash('Login Unsuccessful. Please check Email and password', 'danger')
     return render_template('login.html',title='Login',form=form)
 
+
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
-def save_picture(form_picture):
-  random_hex = secrets.token_hex(8)
-  _, f_ext = os.path.splitext(form_picture.filename)
-  picture_fn = random_hex + f_ext
-  picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-  output_size = (125, 125)
-  i = Image.open(form_picture)
-  i.thumbnail(output_size)
-  i.save(picture_path)
 
-  return picture_fn
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
 
 @app.route("/account",methods=['GET', 'POST'])
 @login_required
@@ -83,22 +89,25 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
 
+
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
-     form=Postform() 
-     if form.validate_on_submit():
+    form=Postform() 
+    if form.validate_on_submit():
        post = Post(title=form.title.data, content=form.content.data, author=current_user)
        db.session.add(post)
        db.session.commit()
        flash('Your Post has been created!', 'success')
        return redirect(url_for('home'))
-     return render_template('create_post.html', title='New Post', form=form, legend='New Post') 
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post') 
+
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
@@ -130,6 +139,7 @@ def delete_post(post_id):
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
 
+
 @app.route("/user/<string:username>")
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
@@ -138,3 +148,44 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(page=page, per_page=3)
     return render_template('user_posts.html', posts=posts, user=user)
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link: {url_for('reset_token', token=token, _external=True)} If you did not make this request then simply ignore this email and no changes will be made.'''
+    #mail.send(msg)
+    # PLEASE REFER COMMENTS SECTION"PT_10" <<NOT TO RUN ON VISUAL STUDIO>>
+    
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
